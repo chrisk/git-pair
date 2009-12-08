76eb6f6ef03679bb24f0ee9641b8a21870df0eda
Chris Kampmeier <chris@kampers.net>
2009-12-08 03:35:14 -0800

    Make sure the tests don't conflict with git-init's own repo by creating the test repo in /tmp

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,29 +1,27 @@
+require 'tmpdir'
 require 'test/unit/assertions'
 World(Test::Unit::Assertions)
 
 
-module TestHelper
-  TMP_PATH  = File.expand_path(File.join(File.dirname(__FILE__), "../../tmp"))
-  REPO_PATH = File.join(TMP_PATH, "repo")
+module RepositoryHelper
+  REPO_PATH = File.join(Dir::tmpdir, "git-pair-test-repo")
+  REPO_GIT_DIR = File.join(REPO_PATH, ".git")
   GIT_PAIR  = File.expand_path(File.join(File.dirname(__FILE__), "../../bin/git-pair"))
 
   def git_pair(options = "")
-    output = ""
-    FileUtils.cd(REPO_PATH) do
-      output = `#{GIT_PAIR} #{options} 2>&1`
-    end
+    output = `GIT_DIR=#{REPO_GIT_DIR} && #{GIT_PAIR} #{options} 2>&1`
     output.gsub(/\e\[\d\d?m/, '')  # strip any ANSI colors
   end
 end
 
-World(TestHelper)
+World(RepositoryHelper)
 
 
 Before do
-  FileUtils.mkdir_p TestHelper::REPO_PATH
-  FileUtils.cd(TestHelper::REPO_PATH) { `git init` }
+  FileUtils.mkdir_p RepositoryHelper::REPO_PATH
+  `GIT_DIR=#{RepositoryHelper::REPO_GIT_DIR} && git init`
 end
 
 After do
-  FileUtils.rm_rf TestHelper::TMP_PATH
+  FileUtils.rm_rf RepositoryHelper::REPO_PATH
 end
