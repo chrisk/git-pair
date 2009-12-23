4befcfc8ad0506b736da716b6fff8a2fb2d1193c
Chris Kampmeier <chris@kampers.net>
2009-12-23 00:09:03 -0500

    In the tests, back up this repo's .git/config as well as ~/.gitconfig
    Also rename a bunch of constants for clarity.

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,28 +5,32 @@ World(Test::Unit::Assertions)
 
 module RepositoryHelper
   # TODO: use 1.8.7's Dir.mktmpdir?
-  REPO_PATH = File.join(Dir::tmpdir, "git-pair-test-repo")
-  REPO_GIT_DIR = File.join(REPO_PATH, ".git")
-  GIT_PAIR  = File.expand_path(File.join(File.dirname(__FILE__), "../../bin/git-pair"))
+  TEST_REPO_PATH         = File.join(Dir::tmpdir, "git-pair-test-repo")
+  TEST_REPO_DOT_GIT_PATH = "#{TEST_REPO_PATH}/.git"
+
+  PROJECT_PATH       = File.join(File.dirname(__FILE__), "../..")
+  GIT_PAIR           = "#{PROJECT_PATH}/bin/git-pair"
+  CONFIG_BACKUP_PATH = "#{PROJECT_PATH}/tmp"
 
   def git_pair(options = "")
-    output = `GIT_DIR=#{REPO_GIT_DIR} && #{GIT_PAIR} #{options} 2>&1`
+    output = `GIT_DIR=#{TEST_REPO_DOT_GIT_PATH} && #{GIT_PAIR} #{options} 2>&1`
     output.gsub(/\e\[\d\d?m/, '')  # strip any ANSI colors
   end
 
   def git_config
-    `GIT_DIR=#{REPO_GIT_DIR} && git config --list 2>&1`
+    `GIT_DIR=#{TEST_REPO_DOT_GIT_PATH} && git config --list 2>&1`
   end
 
-  def backup_global_gitconfig
-    FileUtils.cp File.expand_path('~/.gitconfig'),
-                 File.expand_path('~/.gitconfig.bak')
+  def backup_gitconfigs
+    FileUtils.mkdir_p CONFIG_BACKUP_PATH
+    FileUtils.cp File.expand_path("~/.gitconfig"), "#{CONFIG_BACKUP_PATH}/.gitconfig.backup"
+    FileUtils.cp "#{PROJECT_PATH}/.git/config", "#{CONFIG_BACKUP_PATH}/config.backup"
   end
 
-  def restore_global_gitconfig
-    FileUtils.cp File.expand_path('~/.gitconfig.bak'),
-                 File.expand_path('~/.gitconfig')
-    FileUtils.rm File.expand_path('~/.gitconfig.bak')
+  def restore_gitconfigs
+    FileUtils.cp "#{CONFIG_BACKUP_PATH}/config.backup",     "#{PROJECT_PATH}/.git/config"
+    FileUtils.cp "#{CONFIG_BACKUP_PATH}/.gitconfig.backup", File.expand_path("~/.gitconfig")
+    FileUtils.rm_rf CONFIG_BACKUP_PATH
   end
 end
 
@@ -34,12 +38,12 @@ World(RepositoryHelper)
 
 
 Before do
-  backup_global_gitconfig
-  FileUtils.mkdir_p RepositoryHelper::REPO_PATH
-  `GIT_DIR=#{RepositoryHelper::REPO_GIT_DIR} && git init`
+  backup_gitconfigs
+  FileUtils.mkdir_p RepositoryHelper::TEST_REPO_PATH
+  `GIT_DIR=#{RepositoryHelper::TEST_REPO_DOT_GIT_PATH} && git init`
 end
 
 After do
-  FileUtils.rm_rf RepositoryHelper::REPO_PATH
-  restore_global_gitconfig
+  FileUtils.rm_rf RepositoryHelper::TEST_REPO_PATH
+  restore_gitconfigs
 end
